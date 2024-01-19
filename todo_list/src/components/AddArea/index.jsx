bd221819d06bafde29ca3b389615012b552c8ac0
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-01-19 20:46:06 -0300

    a

--- todo_list/src/components/AddArea/index.jsx
+++ todo_list/src/components/AddArea/index.jsx
@@ -1,13 +1,26 @@
 import * as S from './styles'
+import { useState, KeyboardEvent } from 'react'
 
 export const AddArea = () => {
+
+    const [ inputText, setInputText ] = useState('')
+
+    function handleKeyUp (e) {
+        if (e.code === 'Enter') {
+            
+        }
+    }
 
     return (
         <S.Container>
             <div className='image'>➕</div>
             <input 
                 type="text"
-                placeholder='Adicione uma tarefa' />
+                placeholder='Adicione uma tarefa'
+                value={inputText}
+                onChange={e => setInputText(e.target.value)}
+                onKeyUp={handleKeyUp}
+                />
             
         </S.Container>
 

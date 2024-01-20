1d6c9eb4897c57535dd752928ff7ef7acd369436
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-01-20 09:47:57 -0300

    add function checked object true

--- todo_list/src/App.jsx
+++ todo_list/src/App.jsx
@@ -1,11 +1,11 @@
+// App.js
 import * as S from './App.styles';
 import { useState } from 'react';
-import ListItem from './components/ListItem/index'
+import ListItem from './components/ListItem/index';
 import { AddArea } from './components/AddArea';
 
 function App () {
-
-  const [ list, setList ] = useState([
+  const [list, setList] = useState([
     {
       id: 1,
       name: "Comprar o pão",
@@ -18,34 +18,41 @@ function App () {
     },
   ]);
 
+  // Modificação na função handleAddTask para calcular o próximo ID corretamente
   const handleAddTask = (taskName) => {
-    let newList = [...list];
-    newList.push({
-      id: list.length++,
-      name: taskName,
-      done: false
-    })
-    setList(newList)
-  }
+    setList(prevList => [
+      ...prevList,
+      {
+        id: prevList.length > 0 ? prevList[prevList.length - 1].id + 1 : 1,
+        name: taskName,
+        done: false,
+      },
+    ]);
+  };
+
+  // Adição da função handleCheckboxChange para lidar com a mudança no checkbox
+  const handleCheckboxChange = (taskId, isChecked) => {
+    setList((prevList) =>
+      prevList.map((item) =>
+        item.id === taskId ? { ...item, done: isChecked } : item
+      )
+    );
+  };
 
   return (
-    <S.Container >
+    <S.Container>
       <S.Area>
         <S.Header>Lista de Tarefas</S.Header>
 
-        <AddArea onEnter={handleAddTask}/>
+        <AddArea onEnter={handleAddTask} />
 
-
-
-        {list.map((item, index) =>(
-          
-          <ListItem key={index} item={item}/>
-          
+        {list.map((item, index) => (
+          // Adição da propriedade onCheckboxChange para o componente ListItem
+          <ListItem key={index} item={item} onCheckboxChange={handleCheckboxChange} />
         ))}
-        
       </S.Area>
     </S.Container>
-  )
+  );
 }
 
-export default App
+export default App;
--- todo_list/src/components/ListItem/index.jsx
+++ todo_list/src/components/ListItem/index.jsx
@@ -1,23 +1,31 @@
-import { useState } from "react";
+// ListItem.js
+import React, { useState } from "react";
 import * as S from "./styles";
 
-const ListItem = ({ item }) => {
-	const [isChecked, setIsChecked] = useState(item.done);
+const ListItem = ({ item, onCheckboxChange }) => {
+  const [isChecked, setIsChecked] = useState(item.done);
 
-	return (
-		<S.Container done={isChecked}>
-			<input
-				type="checkbox"
-				name={item.name}
-				id={item.name}
-				checked={isChecked}
-        onChange={e => setIsChecked(e.target.checked)}
-			/>
-			<label htmlFor={item.name}>
-				<span>{item.name}</span>
-			</label>
-		</S.Container>
-	);
+  // Função para lidar com a mudança no checkbox
+  const handleCheckboxChange = (e) => {
+    setIsChecked(e.target.checked);
+    // Chama a função fornecida pelo componente pai (App) para atualizar o estado global
+    onCheckboxChange(item.id, e.target.checked);
+  };
+
+  return (
+    <S.Container done={isChecked}>
+      <input
+        type="checkbox"
+        name={item.name}
+        id={item.name}
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+      />
+      <label htmlFor={item.name}>
+        <span>{item.name} - {item.done.toString()}</span>
+      </label>
+    </S.Container>
+  );
 };
 
 export default ListItem;
